33061dbc67cd9526348e4d3106e01ebe469e89f4
itw <venkateshvishwas12@gmail.com>
2024-09-15 20:35:44 +0530

    webp

--- frontend/food-blog-app/src/pages/Home.jsx
+++ frontend/food-blog-app/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import foodRecipe from '../assets/foodRecipe.png'
+import foodRecipe from '../assets/foodRecipe.webp'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import RecipeItems from '../components/RecipeItems'
